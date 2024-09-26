caf562b8840e89a1e0afdc902fafa8d04e5596ef
LiXo12318 <maksym.koshyn@oa.edu.ua>
2024-09-27 00:08:00 +0300

    Laba5

---
+++ Laba5/src/components/Loading.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div style={styles.container}>
+      <div style={styles.spinner}></div>
+      <p>Завантаження...</p>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  spinner: {
+    width: '50px',
+    height: '50px',
+    border: '6px solid #f3f3f3',
+    borderTop: '6px solid #3498db',
+    borderRadius: '50%',
+    animation: 'spin 1s linear infinite',
+  }
+};
+
+export default Loading;
